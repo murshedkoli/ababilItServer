b876c327c42c4ce3014e776defd1129dbf97a3ba
Murshed Al Main <murshedkoli@gmail.com>
2021-06-06 19:08:50 +0600

    update expense

--- index.js
+++ index.js
@@ -17,92 +17,108 @@ app.use(cors());
 client.connect(err => {
   const studentsCollection = client.db("ababilIt").collection("students");
   const adminCollection = client.db("ababilIt").collection("admin");
+  const expenseCollection = client.db("ababilIt").collection("expenses");
 
 
 
   app.get('/', (req, res) => {
     res.send('Database Conncected')
   })
-  
+
 
   app.get('/students', (req, res) => {
-    studentsCollection.find({confirmAddmission: true})
-    .toArray((err, documents) =>{
-      res.send(documents);
-    })
+    studentsCollection.find({ confirmAddmission: true })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
   })
 
- 
+
   app.get('/studentsapply', (req, res) => {
-    studentsCollection.find({confirmAddmission: false})
-    .toArray((err, documents) =>{
-      res.send(documents);
-    })
+    studentsCollection.find({ confirmAddmission: false })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
   })
 
-  
+  app.get('/expenses', (req, res) => {
+    expenseCollection.find()
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+
   app.get('/admindata', (req, res) => {
-    const email=req.query.email;
-    
-    adminCollection.find({email: email})
-    .toArray((err, documents) =>{
-      res.send(documents);
-      console.log(documents)
-    })
+    const email = req.query.email;
+
+    adminCollection.find({ email: email })
+      .toArray((err, documents) => {
+        res.send(documents);
+        console.log(documents)
+      })
   })
 
-  
+
   app.patch('/confirm/:id', (req, res) => {
-    const id=req.params.id;
-    const data= req.body;
-    
-    studentsCollection.updateOne({_id: ObjectId(id)},
-    {$set:{confirmAddmission: true, paymentAmmount: data.ammount,addmissionDate: data.date }}
+    const id = req.params.id;
+    const data = req.body;
+
+    studentsCollection.updateOne({ _id: ObjectId(id) },
+      { $set: { confirmAddmission: true, paymentAmmount: data.ammount, addmissionDate: data.date } }
     )
-    .then(result=>{
-      res.send(result)
-      console.log(data)
-    })
-   
-   
+      .then(result => {
+        res.send(result)
+      })
+
+
   })
 
-  
-app.post('/addstudent', (req, res)=>{
-  const student = req.body;
-  studentsCollection.insertOne(student)
-  .then(resutl=> {
-    res.send(resutl)
-    console.log(resutl)
+
+  app.post('/addstudent', (req, res) => {
+    const student = req.body;
+    studentsCollection.insertOne(student)
+      .then(resutl => {
+        res.send(resutl)
+        console.log(resutl)
+      })
+
   })
 
-})
+  app.post('/addexpense', (req, res) => {
+    const expense = req.body;
+    expenseCollection.insertOne(expense)
+      .then(resutl => {
+        res.send(resutl)
+      })
 
-
-app.post('/admin', (req, res)=>{
-  
-  adminCollection.insertOne(req.body)
-  .then(resutl=> {
-    res.send(resutl)
-    console.log(resutl)
   })
 
-})
 
+  app.post('/admin', (req, res) => {
 
-app.post('/admin/login', (req, res)=>{
-  const admin = req.body;
-  console.log(admin)
-  adminCollection.find({email: admin.email, password: admin.password})
-  .toArray((err, document)=>{
-    res.send(document[0]);
-    console.log(document)
+    adminCollection.insertOne(req.body)
+      .then(resutl => {
+        res.send(resutl)
+        console.log(resutl)
+      })
+
   })
 
-})
+
+  app.post('/admin/login', (req, res) => {
+    const admin = req.body;
+    console.log(admin)
+    adminCollection.find({ email: admin.email, password: admin.password })
+      .toArray((err, document) => {
+        res.send(document[0]);
+        console.log(document)
+      })
+
+  })
 
 
-console.log('connected')
+  console.log('connected')
 
   // client.close();
 });
